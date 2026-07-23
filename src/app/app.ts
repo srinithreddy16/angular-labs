3b2947af86de027bf87ad45b346b2d3f6d097353
Srinith Reddy <srinithreddy2002@gmail.com>
2026-07-23 12:01:32 -0400

    Set up Angular project with Bootstrap integration and UI components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Navbar } from './components/navbar/navbar';
+import { Categories } from './components/categories/categories';
+import { Carousel } from './components/carousel/carousel';
+import { Body } from './components/body/body';
+import { Footer } from './components/footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Categories, Carousel, Body, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(App, appConfig) //bootstraping(which component to load, which configuration to load)
   .catch((err) => console.error(err));
